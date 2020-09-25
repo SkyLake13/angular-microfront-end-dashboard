9df4cafd234e89eac458b9244e6c81235bd421bc
kumawabh <kumawabh@WL350183>
2020-09-25 20:54:52 +0530

    resizable div

--- src/app/main-grid/main-grid.component.ts
+++ src/app/main-grid/main-grid.component.ts
@@ -5,11 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './main-grid.component.html',
   styleUrls: ['./main-grid.component.scss']
 })
-export class MainGridComponent implements OnInit {
+export class MainGridComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public openStreet = `https://www.openstreetmap.org/export/embed.html?
+  bbox=-0.004017949104309083%2C51.47612752641776%2C0
+  .00030577182769775396%2C51.478569861898606&layer=mapnik`;
 }
